834ecd08dbfdf20f67206e577630641f51bb6063
romanbishof <romanbishof@gmail.com>
2021-10-24 13:44:06 +0300

    navbar complete

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './navbar.scss'
+import {HomeRounded} from '@material-ui/icons';
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="left">
+                    <img src="https://blog.herolo.co.il/wp-content/uploads/Herolo_Logo.png" 
+                    alt=''/>
+                </div>
+
+                <div className="right">
+                    <HomeRounded className="homePage"/>
+                    <span>Favorites</span>
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
